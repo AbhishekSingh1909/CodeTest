caadc4f1627c4228767175cf3f4af30361cb24e1
AbhishekSingh1909 <103984974+AbhishekSingh1909@users.noreply.github.com>
2022-04-19 11:57:39 +0530

    Code Test files
    1. MVC Application
    2. Code API

---
+++ CodeTestAPI/Controllers/TestController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Web.Http.Cors;
+
+namespace CodeTestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    
+    public class TestController : ControllerBase
+    {
+        [HttpGet("SumAndCheckPrimeNumbers/{numbers}")]
+        public IActionResult GetSumAndPrimeNumber([System.Web.Http.FromUri] int [] numbers)
+        {
+            int count = 0;
+            int sum = numbers.Sum(x => x);
+            for ( int i = 1; i <= sum; i++)
+            {
+                if (sum % i == 0)
+                {
+                    count++;
+                }
+            }
+
+            if (count == 2)
+            {
+                return Ok(new { result=sum, isprime = true });
+
+            }
+
+            return Ok(new { result = sum, isprime = false});
+        }
+
+        [HttpGet ("check_prime_number/{num}")]
+        public IActionResult GetPrimeNumber(int num)
+        {
+            
+            int count = 0;
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    count++;
+                }
+            }
+
+            if (count== 2)
+            {
+                return Ok(new { isprime = true });
+            }
+
+            return Ok(new { isprime = false });
+        }
+
+    }
+}
---
+++ CodeTestAPI/Numbers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeTestAPI
+{
+    public class Parametrs
+    {
+        public int[] numbres { get; set; }
+    }
+}
